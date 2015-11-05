a9db0afa75e57d6baae5928445e8eccf3ec26459
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-11-05 00:23:22 -0200

    Homework 1 - Exercise 4

---
+++ haskell/HW01/exercise4.hs
@@ -0,0 +1,11 @@
+--  double (double 2)
+-- = { applying the inner double }
+-- double (2 + 2)
+-- = { applying double }
+-- (2 + 2) + (2 + 2)
+-- = { applying the first + }
+-- 4 + (2 + 2)
+-- = { applying the second + }
+-- 4 + 4
+-- = { applying + }
+-- 8
